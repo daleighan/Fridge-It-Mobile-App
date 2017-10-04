54273f7f52b9f7e00ce5df5d7adce8f31f8c71c8
David Leigh <daleighan@gmail.com>
2017-10-04 11:47:16 -0700

    NativeRouter now routes on the fixedmenu page

--- fridgeItMobile/component/Landing_Page/fixedMenu.js
+++ fridgeItMobile/component/Landing_Page/fixedMenu.js
@@ -8,21 +8,27 @@ import {
 } from 'react-native';
 import { Router, Route, Link, Redirect, NativeRouter } from 'react-router-native';
 
-const FixedMenu = (props) => (		
+const FixedMenu = (props) => {	
+	return(	
 	<View>				
-		<Image
-          style={{width: 50, height: 50}}
-          source={{uri: 'https://cdn0.iconfinder.com/data/icons/household-appliances-icons-set-cartoon-style/512/a672-512.png'}}
-        />
     <NativeRouter>
     	<View>
     		<Image
           style={{width: 50, height: 50}}
           source={{uri: 'https://cdn0.iconfinder.com/data/icons/household-appliances-icons-set-cartoon-style/512/a672-512.png'}}
         />
+        <Link to="/login">
+        	<Text>Login</Text>
+        </Link>
+        <Link to="/signup">
+        	<Text>Logout</Text>
+        </Link>
+        <Route path="/login" render={() => (<Text>Login</Text>)} />
+        <Route path="/signup" render={() => (<Text>Logout</Text>)} />
     	</View>
     </NativeRouter>
 	</View>
-)
+	)
+}
 
 export default FixedMenu;
