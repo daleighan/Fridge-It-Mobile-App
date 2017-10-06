2d9cd94d106fc48cf711634ae004e898f05c9b13
David Leigh <daleighan@gmail.com>
2017-10-06 00:55:47 -0700

    Added a conditional render for the itemListView

--- fridgeItMobile/component/Fridge/itemListView.js
+++ fridgeItMobile/component/Fridge/itemListView.js
@@ -11,17 +11,23 @@ import {
 } from 'react-native';
 
 const ItemList = (props) => {
-	console.log('itemlistprops', props);
-	return (
-		<View>
-			{props.items.map(item => {
-				return( 
-				<Text key={item.id}>{item.name}{item.quantity} </Text>
-				)
-			})
-			}
-		</View>
-	)	
+	
+	if (props.items.length) {
+		return (
+			<View>
+				{props.items.map(item => {
+					return( 
+					<Text key={item.id}>{item.name}{item.quantity} </Text>
+					)
+				})
+				}
+			</View>
+		)
+	} else {
+		return (
+			<Text>No Items in This Category</Text>
+		)
+	}
 }
 
 export default ItemList;
